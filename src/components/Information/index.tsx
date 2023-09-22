ab1ba27b47be654519960536fb267a38acd66aed
eunjiji <wqwq1563@gmail.com>
2023-09-22 16:59:43 +0900

    classname change

--- src/components/Information/index.tsx
+++ src/components/Information/index.tsx
@@ -7,8 +7,8 @@ const Information = ({information}: Pick<DataProps, "information">) => {
     return (
         <div className="flex flex-col gap-8">
             <div className="flex flex-col gap-2">
-                <div style={{marginBottom: "5%"}}>
-                    <div style={{
+                <div className="relative flex shrink gap-8 items-start" style={{marginBottom: "5%"}}>
+                    <div className="hidden md:block relative w-40 shrink-0 h-40" style={{
                         width: "18%",
                         marginRight: "5%",
                         float: "left"
@@ -18,23 +18,26 @@ const Information = ({information}: Pick<DataProps, "information">) => {
                             objectPosition: "top"
                         }}></img>
                     </div>
-                    <h1 className="leading-[1.15]">
-                        안녕하세요,
-                        <br/> Back-end 개발자{" "}
-                        <br/>
-                        <span className="text-PRIMARY font-semibold">{information.name}</span>
-                        입니다.
-                    </h1>
-                    <div className="flex gap-1">
-                        {information.contact.map((contact) => (
-                            <ContactItem
-                                key={contact.id}
-                                className="text-BLACK hover:text-PRIMARY_HEAVY dark:hover:text-PRIMARY_HEAVY"
-                                {...contact}
-                            >
-                                {contact.name}
-                            </ContactItem>
-                        ))}
+                    <div className="flex flex-col w-full">
+                        <h1 className="leading-[1.15]">
+                            안녕하세요,
+                            <br/> Back-end 개발자{" "}
+                            <br/>
+                            <span className="text-PRIMARY font-semibold">{information.name}</span>
+                            입니다.
+                        </h1>
+                        <div className="hidden md:flex flex-col gap-4"></div>
+                        <div className="flex px-4 gap-1">
+                            {information.contact.map((contact) => (
+                                <ContactItem
+                                    key={contact.id}
+                                    className="text-BLACK hover:text-PRIMARY_HEAVY dark:hover:text-PRIMARY_HEAVY"
+                                    {...contact}
+                                >
+                                    {contact.name}
+                                </ContactItem>
+                            ))}
+                        </div>
                     </div>
                 </div>
                 <Introduce markdown={information.markdown}/>
