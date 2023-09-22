026ec8277a670ec35a761267ee575131f8840064
eunjiji <wqwq1563@gmail.com>
2023-09-22 16:23:36 +0900

    color change

--- src/components/Information/index.tsx
+++ src/components/Information/index.tsx
@@ -9,7 +9,7 @@ const Information = ({information}: Pick<DataProps, "information">) => {
             <div className="flex flex-col gap-2">
                 <div style={{marginBottom: "5%"}}>
                     <div style={{
-                        width: "15%",
+                        width: "18%",
                         marginRight: "5%",
                         float: "left"
                     }}>
@@ -19,9 +19,9 @@ const Information = ({information}: Pick<DataProps, "information">) => {
                         }}></img>
                     </div>
                     <h1 className="leading-[1.15]">
-                        {/* 포지션에 맞게 문구를 수정해주세요. 혹은, 본인이 원하는 대로 문구를 바꿔주세요. */}
                         안녕하세요,
-                        <br/> 백엔드 개발자{" "}
+                        <br/> Back-end 개발자{" "}
+                        <br/>
                         <span className="text-PRIMARY font-semibold">{information.name}</span>
                         입니다.
                     </h1>
